a4cfd3cd0b7c83ea51a18b0941f867c915642dc1
longwei <longwei@longwei-OptiPlex-780.(none)>
2013-01-31 17:28:26 -0500

    1.11

--- ch1/exe.rkt
+++ ch1/exe.rkt
@@ -27,3 +27,53 @@
  ;;ex1.5
  ;;using normal order eval, which is fully expand and then reduce, will infinite loop
  ;;while applicative-order eval, will short-circuit and produce 0
+ 
+ 
+ ;;ex1.6
+ ;;as new-if use normal order evluation, the rewritten sqrt=iter 
+ ;;never terminates.
+ 
+ ;;ex1.7
+ ;;use relative value
+ (define (good-enough? guess x)
+   (< (abs (-(square guess) x)) (* 0.001 guess)))
+ 
+ ;;ex1.8
+(define (square x)(* x x)) 
+(define (cube x) (* x x x))
+
+(define (good-enough? guess x)
+   (< (abs (-(cube guess) x)) (* 0.001 guess)))
+ (define (improve guess x)
+	(/ (+ (/ x (square guess)) (* 2 guess)) 3) ) 
+
+ (define (cube-root-iter guess x)
+ 	(if (good-enough? guess x) 
+ 		guess
+ 		(cube-root-iter (improve guess x) x)))
+ 
+ (define (cube-root x) (cube-root-iter 1 x))
+
+ ;;ex 1.9
+ ;;first one is recursive, 
+ ;;second is iterative
+
+ ;;ex 1.10
+ ;;normal
+
+ ;;ex 1.11
+ (define (f n)
+ 	(if (< n 3) n 
+ 		(+ ( f (- n 1)) (* 2 ( f ( - n 2))) (* 3 (f (- n 3))))
+ 		)
+ 	)
+
+ (foo 7)
+ ;;iterative
+  (define (foo n) 
+   (define (iter a b c count) 
+     (if (= count 3) 
+       (+ c (* 2 b) (* 3 a))
+       (iter b c (+ c (* 2 b) (* 3 a)) (- count 1)))) 
+   (iter 0 1 2 n)) 
+;;moving 3slot rule -> and fill out the form
